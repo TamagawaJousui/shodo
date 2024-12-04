2acfe0b541c80ed6241108c4dd329fb9e5c076cf
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-12-04 11:16:05 +0900

    from div to section, refine css

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import making_1 from "./assets/making_1.webp";
 import making_2 from "./assets/making_2.webp";
 import making_3 from "./assets/making_3.webp";
 import making_4 from "./assets/making_4.webp";
+import { LuExternalLink } from "react-icons/lu";
 
 function App() {
   return (
@@ -22,10 +23,12 @@ function App() {
           </div>
         </div>
       </div>
+      {/* full width container */}
       <div className="flex w-full justify-center">
+        {/* inner container */}
         <div className="mx-10 max-w-screen-2xl sm:mx-14">
           {/* description */}
-          <div className="page-content  mt-24">
+          <section className="page-content">
             <p>
               本作品は、視覚、聴覚、触覚を通じて、書道の本質である「過程表現」の価値を体感できる体験型作品です。
             </p>
@@ -44,91 +47,100 @@ function App() {
               ―筆意や書き手の感情の流れ―
               の再発見を促し、現代の書道へのイメージに挑戦する革新的な試みとなるでしょう。
             </p>
-          </div>
+          </section>
           {/* description video */}
-          <iframe
-            className=" mt-24 aspect-video w-[stretch] rounded-xl"
-            src="https://www.youtube.com/embed/_RrR8aV4kN4"
-            title="YouTube video player"
-            allow="picture-in-picture"
-            allowFullScreen
-          />
+          <section>
+            <iframe
+              className="mt-24 aspect-video w-[stretch] rounded-xl"
+              src="https://www.youtube.com/embed/_RrR8aV4kN4"
+              title="YouTube video player"
+              allow="picture-in-picture"
+              allowFullScreen
+            />
+          </section>
           {/* visual */}
-          <div className=" mt-24 text-center text-4xl">視覚</div>
-          <div className=" mt-14 flex flex-col gap-8 md:flex-row">
-            <div className="w-full md:flex md:flex-[45%] md:flex-col md:items-center md:justify-center">
-              <img
-                src={description_color}
-                alt="description_color"
-                className="fade-edges  rounded-xl"
-              />
+          <section>
+            <div className="text-center text-4xl">視覚</div>
+            <div className="mt-14 flex flex-col gap-8 md:flex-row">
+              <div className="w-full md:flex md:flex-[45%] md:flex-col md:items-center md:justify-center">
+                <img
+                  src={description_color}
+                  alt="description_color"
+                  className="fade-edges  rounded-xl"
+                />
+              </div>
+              <div className="page-content md:flex-[55%]">
+                <p>
+                  こちらは筆のスピードを色で表現したものです。
+                  <br />
+                  スピードが遅い部分は青、速くなるにつれて緑、黄、オレンジ、赤へとグラデーションで描かれています。
+                </p>
+                <p>
+                  色が速度を示していることを理解すると、視覚的な楽しさに加え、起筆から終筆までの流れを目で追うことで、書のリズムを感じ取ることができます。
+                </p>
+                <p>
+                  たとえば、書の二筆目は「トン、スー、トン」の三折法で構成されていますが、動の二筆目は「トン、スー」の二折法が用いられています。
+                </p>
+                <p>
+                  このように色から全体の動きを詳細に把握することで、書き手の表現へのこだわりや、書による表現の奥深さへの洞察が深まるでしょう。
+                </p>
+              </div>
             </div>
-            <div className="page-content md:flex-[55%]">
-              <p>
-                こちらは筆のスピードを色で表現したものです。
-                <br />
-                スピードが遅い部分は青、速くなるにつれて緑、黄、オレンジ、赤へとグラデーションで描かれています。
-              </p>
-              <p>
-                色が速度を示していることを理解すると、視覚的な楽しさに加え、起筆から終筆までの流れを目で追うことで、書のリズムを感じ取ることができます。
-              </p>
-              <p>
-                たとえば、書の二筆目は「トン、スー、トン」の三折法で構成されていますが、動の二筆目は「トン、スー」の二折法が用いられています。
-              </p>
-              <p>
-                このように色から全体の動きを詳細に把握することで、書き手の表現へのこだわりや、書による表現の奥深さへの洞察が深まるでしょう。
-              </p>
-            </div>
-          </div>
+          </section>
           {/* auditory */}
-          <div className=" mt-24 text-center text-4xl">聴覚</div>
-          <div className=" mt-14 flex flex-col gap-8 md:flex-row">
-            <div className="w-full md:flex md:flex-[45%] md:flex-col md:items-center md:justify-center">
-              <iframe
-                className="aspect-video w-[stretch] rounded-xl"
-                src="https://www.youtube.com/embed/zN-Z_V6UUe0"
-                title="YouTube video player"
-                allow="picture-in-picture"
-                allowFullScreen
-              />
+          <section>
+            <div className="text-center text-4xl">聴覚</div>
+            <div className="mt-14 flex flex-col gap-8 md:flex-row">
+              <div className="w-full md:flex md:flex-[45%] md:flex-col md:items-center md:justify-center">
+                <iframe
+                  className="aspect-video w-[stretch] rounded-xl"
+                  src="https://www.youtube.com/embed/zN-Z_V6UUe0"
+                  title="YouTube video player"
+                  allow="picture-in-picture"
+                  allowFullScreen
+                />
+              </div>
+              <div className="page-content md:flex-[55%]">
+                <p>続いて、書き手の筆運びを音で表現したものです。</p>
+                <p>
+                  書き手自身が筆を運ぶ過程の感覚を、力強い起筆や終筆は和太鼓の音で、流れるような運筆は波の音で再現しました。
+                </p>
+                <p>
+                  筆の動きが生む強弱や間の取り方が音で表現されることで、書き手の呼吸に寄り添うような没入感が生まれます。
+                </p>
+                <p>
+                  音を通じて、視覚では捉えきれない微妙なリズムや筆の躍動感を感じ取ることができ、書き手の動きをより立体的に捉えられます。
+                </p>
+              </div>
             </div>
-            <div className="page-content md:flex-[55%]">
-              <p>続いて、書き手の筆運びを音で表現したものです。</p>
-              <p>
-                書き手自身が筆を運ぶ過程の感覚を、力強い起筆や終筆は和太鼓の音で、流れるような運筆は波の音で再現しました。
-              </p>
-              <p>
-                筆の動きが生む強弱や間の取り方が音で表現されることで、書き手の呼吸に寄り添うような没入感が生まれます。
-              </p>
-              <p>
-                音を通じて、視覚では捉えきれない微妙なリズムや筆の躍動感を感じ取ることができ、書き手の動きをより立体的に捉えられます。
-              </p>
-            </div>
-          </div>
+          </section>
           {/* tactile */}
-          <div className=" mt-24 text-center text-4xl">触覚</div>
-          <div className=" mt-14 flex flex-col gap-16">
-            <div className="page-content">
-              <p>
-                そして、こちらは筆が進む方向と速度を触覚で表現したものです。
-              </p>
-              <p>
-                筆の進む方向に応じて天板が傾き、その傾きの大きさで速度を体感できる仕組みになっています。鑑賞者はこれを触ることで、筆運びの感覚を直接味わうことができます。
-              </p>
-              <p>
-                書き手の筆運びを、時間と空間を超えて「今ここ」で共有することで、書き手の存在が生き生きと想起され、まるで同じ空間で創作の瞬間を分かち合っているかのような体験を得ることができます。
-              </p>
+          <section>
+            <div className="text-center text-4xl">触覚</div>
+            <div className="mt-14 flex flex-col gap-16">
+              <div className="page-content">
+                <p>
+                  そして、こちらは筆が進む方向と速度を触覚で表現したものです。
+                </p>
+                <p>
+                  筆の進む方向に応じて天板が傾き、その傾きの大きさで速度を体感できる仕組みになっています。鑑賞者はこれを触ることで、筆運びの感覚を直接味わうことができます。
+                </p>
+                <p>
+                  書き手の筆運びを、時間と空間を超えて「今ここ」で共有することで、書き手の存在が生き生きと想起され、まるで同じ空間で創作の瞬間を分かち合っているかのような体験を得ることができます。
+                </p>
+              </div>
+              <div className="w-full">
+                <img
+                  src={description_touch}
+                  alt="description_touch"
+                  className="rounded-xl"
+                />
+              </div>
             </div>
-            <div className="w-full">
-              <img
-                src={description_touch}
-                alt="description_touch"
-                className="rounded-xl"
-              />
-            </div>
-          </div>
+          </section>
           {/* summary */}
-          <div className="page-content  mt-24">
+
+          <section className="page-content">
             <p>
               本作品は、書道を長年学び続けてきたメンバーが、書道の鑑賞体験を変えたいという思いから制作したものです。
             </p>
@@ -138,97 +150,114 @@ function App() {
             <p>
               現代社会では、多様なコンテンツに溢れ、一目で理解できる結果を求めがちですが、過程の価値を知るためには、時間をかけてその過程を読み解く姿勢が大切なのではないでしょうか。
             </p>
-          </div>
+          </section>
           {/* exhibition */}
-          <div className=" mt-24 text-center text-4xl">展示</div>
-          <div className=" mt-4 flex flex-col items-center whitespace-pre-wrap text-xl">
-            <div className="flex flex-col items-center justify-center gap-1 sm:flex-row sm:gap-2">
-              <span>東京大学制作展 2024</span>
-              <span> 付いて離れて</span>
+          <section>
+            <div className="text-center text-4xl">展示</div>
+            <div className="mt-4 flex flex-col items-center whitespace-pre-wrap text-xl">
+              <a href="https://www.iiiexhibition.com/" target="_blank">
+                <div className="flex flex-col items-center justify-center gap-1 underline sm:mb-1 sm:flex-row sm:gap-2 sm:border-b sm:border-black sm:pb-0 sm:no-underline">
+                  <span>
+                    <LuExternalLink className="mr-1 inline-block text-xl" />
+                    東京大学制作展 2024
+                  </span>
+                  <span> 付いて離れて</span>
+                </div>
+              </a>
+              <span className="mt-2 w-fit sm:mt-0">11.7 - 11.11</span>
             </div>
-            <span className="mt-2 w-fit sm:mt-0">11.7 - 11.11</span>
-          </div>
-          <div className=" mt-14 flex flex-col gap-12 drop-shadow-2xl">
-            <img src={exhibition_1} alt="exhibition_1" className="rounded-xl" />
-            <div className="flex flex-col gap-12 md:flex-row md:justify-between md:gap-0">
+            <div className="mt-14 flex flex-col gap-12 drop-shadow-2xl">
               <img
-                src={exhibition_2}
-                alt="exhibition_2"
-                className="rounded-xl object-contain md:w-1/4"
+                src={exhibition_1}
+                alt="exhibition_1"
+                className="rounded-xl"
               />
-              <img
-                src={exhibition_3}
-                alt="exhibition_3"
-                className="rounded-xl object-contain md:w-[68%]"
-              />
+              <div className="flex flex-col gap-12 md:flex-row md:justify-between md:gap-0">
+                <img
+                  src={exhibition_2}
+                  alt="exhibition_2"
+                  className="rounded-xl object-contain md:w-1/4"
+                />
+                <img
+                  src={exhibition_3}
+                  alt="exhibition_3"
+                  className="rounded-xl object-contain md:w-[68%]"
+                />
+              </div>
             </div>
-          </div>
+          </section>
           {/* making */}
-          <div className=" mt-24 text-center text-4xl">制作</div>
-          <div className=" mt-14 flex flex-col gap-12 drop-shadow-2xl">
-            <div className="flex flex-col gap-12 md:flex-row md:justify-between md:gap-0">
-              <img
-                src={making_1}
-                alt="making_1"
-                className="rounded-xl object-contain md:w-[73%]"
-              />
-              <img
-                src={making_2}
-                alt="making_2"
-                className="rounded-xl object-contain  md:w-[23%]"
-              />
+          <section>
+            <div className="text-center text-4xl">制作</div>
+            <div className="mt-14 flex flex-col gap-12 drop-shadow-2xl">
+              <div className="flex flex-col gap-12 md:flex-row md:justify-between md:gap-0">
+                <img
+                  src={making_1}
+                  alt="making_1"
+                  className="rounded-xl object-contain md:w-[73%]"
+                />
+                <img
+                  src={making_2}
+                  alt="making_2"
+                  className="rounded-xl object-contain  md:w-[23%]"
+                />
+              </div>
+              <div className="flex flex-col gap-12 md:flex-row md:justify-between md:gap-0">
+                <img
+                  src={making_3}
+                  alt="making_3"
+                  className="rounded-3xl object-contain md:w-[26%]"
+                />
+                <img
+                  src={making_4}
+                  alt="making_4"
+                  className="rounded-xl object-contain md:w-[69.5%]"
+                />
+              </div>
             </div>
-            <div className="flex flex-col gap-12 md:flex-row md:justify-between md:gap-0">
-              <img
-                src={making_3}
-                alt="making_3"
-                className="rounded-3xl object-contain md:w-[26%]"
-              />
-              <img
-                src={making_4}
-                alt="making_4"
-                className="rounded-xl object-contain md:w-[69.5%]"
-              />
-            </div>
-          </div>
+          </section>
           {/* members */}
-          <div className=" mt-24 text-center text-4xl">メンバー</div>
-          <div className=" mt-8 flex flex-col items-center justify-center gap-2 text-xl">
-            <div className="member">
-              <span>小西優多郎</span>
-              <span>
-                東京大学大学院学際情報学府
-                <br className="block md:hidden" /> 修士課程1年
-              </span>
+          <section>
+            <div className="text-center text-4xl">メンバー</div>
+            <div className="mt-8 flex flex-col items-center justify-center">
+              <div className="w-full max-w-72 sm:max-w-lg">
+                <div className="member">
+                  <span>小西優多郎</span>
+                  <span>
+                    東京大学大学院学際情報学府
+                    <br className="block sm:hidden" /> 修士課程1年
+                  </span>
+                </div>
+                <div className="member">
+                  <span>田中祐玖</span>
+                  <span>
+                    東京大学大学院学際情報学府
+                    <br className="block sm:hidden" /> 修士課程1年
+                  </span>
+                </div>
+                <div className="member">
+                  <span>都築あい</span>
+                  <span>
+                    東京大学大学院情報理工学系
+                    <br className="block sm:hidden" /> 修士課程1年
+                  </span>
+                </div>
+                <div className="member">
+                  <span>Xi Jingfan</span>
+                  <span>
+                    東京大学大学院情報理工学系
+                    <br className="block sm:hidden" /> 修士課程1年
+                  </span>
+                </div>
+              </div>
             </div>
-            <div className="member">
-              <span>田中祐玖</span>
-              <span>
-                東京大学大学院学際情報学府
-                <br className="block md:hidden" /> 修士課程1年
-              </span>
-            </div>
-            <div className="member">
-              <span>都築あい</span>
-              <span>
-                東京大学大学院情報理工学系
-                <br className="block md:hidden" /> 修士課程1年
-              </span>
-            </div>
-            <div className="member">
-              <span>Xi Jingfan</span>
-              <span>
-                東京大学大学院情報理工学系
-                <br className="block md:hidden" /> 修士課程1年
-              </span>
-            </div>
-          </div>
+          </section>
           {/* spacer */}
-          <div className=" mt-20 text-center text-4xl" />
+          <section className="mt-20 text-center text-4xl" />
           {/* footer */}
-          <div className=" border-t border-black pb-16 pt-10 text-center font-copyright text-sm">
+          <section className="border-t border-black pb-16 pt-10 text-center font-copyright text-sm">
             Copyright ©︎ 2024 Trajectory of Handwriting.
-          </div>
+          </section>
         </div>
       </div>
     </>
